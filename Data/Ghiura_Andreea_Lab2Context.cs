25c809ae7c854fcbd16b9fe4cc2d628ea02e17b6
Andreea <Andreea@CALLIS>
2022-11-01 15:57:04 +0200

    Add project files.

---
+++ Data/Ghiura_Andreea_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ghiura_Andreea_Lab2.Models;
+
+namespace Ghiura_Andreea_Lab2.Data
+{
+    public class Ghiura_Andreea_Lab2Context : DbContext
+    {
+        public Ghiura_Andreea_Lab2Context (DbContextOptions<Ghiura_Andreea_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ghiura_Andreea_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Ghiura_Andreea_Lab2.Data;
+using Ghiura_Andreea_Lab2.Models;
+
+namespace Ghiura_Andreea_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Ghiura_Andreea_Lab2.Data.Ghiura_Andreea_Lab2Context _context;
+
+        public IndexModel(Ghiura_Andreea_Lab2.Data.Ghiura_Andreea_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Book != null)
+            {
+                Book = await _context.Book.ToListAsync();
+            }
+        }
+    }
+}
